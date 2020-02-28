3465f852ac9166566ea088e98574c3252c64599a
LeoRoi <kuzne4eg@gmail.com>
2020-02-28 21:40:27 +0100

    feedback timestamp and browser

--- src/main/java/home/lflt/model/Feedback.java
+++ src/main/java/home/lflt/model/Feedback.java
@@ -1,15 +1,14 @@
 package home.lflt.model;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,25 +29,30 @@ public class Feedback {
 
 //    private String url;
 
-
     @NotNull(message = INPUT_REQUIRED)
     @NotBlank(message = INPUT_REQUIRED)
     private String device;
 
     @NotNull(message = INPUT_REQUIRED)
     @NotBlank(message = INPUT_REQUIRED)
-    private String os;
+    private String browser;
 
     @NotNull(message = INPUT_REQUIRED)
     @NotBlank(message = INPUT_REQUIRED)
+    @Column(columnDefinition="TEXT")
     private String msg;
 
     @NotNull(message = INPUT_REQUIRED)
     @NotBlank(message = INPUT_REQUIRED)
     private String name;
 
-//    @Email
+    @NotNull(message = INPUT_REQUIRED)
     @Pattern(regexp = "(?:[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", message = INPUT_REQUIRED)
     @NotBlank(message = INPUT_REQUIRED)
     private String email;
+
+//    @NotNull
+//    @Column(columnDefinition="TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    private Date timestamp;
 }
